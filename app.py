2742b4187464b8e279066ca423082c34708b73aa
MARCORYKER <thangadanchajairohithyadav@gmail.com>
2025-04-11 00:02:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -335,5 +335,5 @@ def upload_file():
     return jsonify(result)
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  # Render provides PORT
-    app.run(host="0.0.0.0", port=port, debug=False)  # Bind to 0.0.0.0 for external access
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+
